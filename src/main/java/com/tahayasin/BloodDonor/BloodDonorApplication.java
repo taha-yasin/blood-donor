62e0a6a5ac2f6d29a1630c8571b5567fd0523f87
tahayasin <gulamtahayaseen@gmail.com>
2022-02-10 20:47:57 +0530

    initial project setup with spring boot

---
+++ src/main/java/com/tahayasin/BloodDonor/BloodDonorApplication.java
@@ -0,0 +1,13 @@
+package com.tahayasin.BloodDonor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDonorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodDonorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tahayasin/BloodDonor/BloodDonorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tahayasin.BloodDonor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
